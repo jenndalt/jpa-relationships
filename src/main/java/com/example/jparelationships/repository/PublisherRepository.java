d8f93918fd22eea0a175061f833510aca3ec1f39
jenndalt <jennifer.dalton@cognizant.com>
2021-02-06 14:38:59 -0600

    remove unneeded classes

--- src/main/java/com/example/jparelationships/repository/PublisherRepository.java
+++
@@ -1,7 +0,0 @@
-//package com.example.jparelationships.repository;
-//
-//import com.example.jparelationships.publishing.Publisher;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface PublisherRepository extends JpaRepository<Long, Publisher> {
-//}
--- src/main/java/com/example/jparelationships/university/Teacher.java
+++
@@ -1,4 +0,0 @@
-package com.example.jparelationships.university;
-
-public class Teacher {
-}
