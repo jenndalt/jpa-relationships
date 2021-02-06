4db5d8c2a9674b144ff329b228b83f0b9d602f76
jenndalt <jennifer.dalton@cognizant.com>
2021-02-06 14:41:08 -0600

    add repository annotation

--- src/main/java/com/example/jparelationships/repository/BookRepository.java
+++ src/main/java/com/example/jparelationships/repository/BookRepository.java
@@ -3,8 +3,10 @@ package com.example.jparelationships.repository;
 import com.example.jparelationships.publishing.Author;
 import com.example.jparelationships.publishing.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, UUID> {
 }
